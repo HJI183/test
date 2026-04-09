6e182de591f21be8702a0418e626a2532e06113e
hanjuni <hanjunlee040622u@naver.com>
2026-04-09 10:34:38 +0900

    첫번째 프로그램

---
+++ print.h
@@ -0,0 +1,9 @@
+total 11
+drwxr-xr-x 1 hanju 197609   0 Apr  9 10:26 ./
+drwxr-xr-x 1 hanju 197609   0 Apr  9 09:26 ../
+drwxr-xr-x 1 hanju 197609   0 Apr  9 10:22 .git/
+-rw-r--r-- 1 hanju 197609 113 Apr  9 09:47 README.md
+-rw-r--r-- 1 hanju 197609 357 Apr  9 10:25 list.cpp
+-rw-r--r-- 1 hanju 197609   0 Apr  9 10:26 print.h
+drwxr-xr-x 1 hanju 197609   0 Apr  9 09:45 test/
+-rw-r--r-- 1 hanju 197609 194 Apr  9 10:23 test.txt
